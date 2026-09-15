e47d42b3855cd645fe7a3cfee32a6a25ff6f9b4c
naeemaa149-svg <naeemaa149@gmail.com>
2026-09-15 19:00:47 +0300

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,11 @@
+print("nema : Naima ")
+x = int(input("x="))
+if x >= 90:
+    print("ممتاز")
+elif x >= 80:
+    print("جيد جداً")
+elif x >= 70:
+    print("جيد")
+elif x >= 60:
+    print("متوسط")
+  
